c12126aeb1b206dbb659f7422f829066983644b7
Gnersies <justin.morgan99@yahoo.com>
2016-08-25 11:56:49 -0600

    Name that celebrity project
    The whole folder

---
+++ NameThatCelebrity/name.java
@@ -0,0 +1,25 @@
+
+public class name {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s1 = "Allan Alda";
+		String s2 = "John wayne";
+		String s3 = "Gregory Peck";
+		
+		String a = s1.substring(2, 7);
+		String b = s2.substring(2, 7);
+		String c = s3.substring(2, 9);
+		
+		System.out.print(s1);
+		System.out.print(">>>");
+		System.out.println(a);
+		System.out.print(s2);
+		System.out.print(">>>");
+		System.out.println(b);
+		System.out.print(s3);
+		System.out.print(">>>");
+		System.out.println(c);
+	}
+
+}
